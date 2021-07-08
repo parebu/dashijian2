96fe658058093a98d256c58af520e5ec7bfac838
lmh0823 <lmh0823@163.com>
2021-07-08 12:27:21 +0800

    完成了个人中心开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,43 @@
 $(function() {
-
     getUserInfo()
-
+    var layer = layui.layer
     $('#btn').on('click', function() {
-        layer.confirm('确认退出?', { icon: 3, title: '提示' }, function(index) {
+        layer.confirm('确认退出？', { icon: 3, title: '提示' }, function(index) {
             //do something
 
+            //清空本地存储中的token
             layer.close(index);
             localStorage.removeItem('token')
+                // 重新跳转到登陆页面
             location.href = '/login.html'
+                // 关闭confirm 询问框
+
         })
     })
-
-
-
-
-    function getUserInfo() {
-        $.get('/my/userinfo', function(res) {
-            if (res.status !== 0) {
-                return layui.layer.msg(res.message)
-            }
-
-            layui.layer.msg(res.message)
-            console.log(res);
-            renderAvatar(res.data)
-        })
-    }
-
-    function renderAvatar(data) {
-        var name = data.username || data.nickname
-        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-        if (data.user_pic !== null) {
-            $('.layui-nav-img').attr('src', data.user_pic).val()
-            $('.text-avatar').hide()
-        } else {
-            $('.layui-nav-img').hide()
-            var first = name[0].toUpperCase()
-            $('.text-avatar').html(first).show()
-        }
-    }
 })
+
+
+function getUserInfo() {
+    $.get('/my/userinfo', function(res) {
+        if (res.status !== 0) {
+            return layer.msg(res.message)
+        }
+
+
+
+        renderAvatar(res.data)
+    })
+}
+
+function renderAvatar(data) {
+    var name = data.nickname || data.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (data.user_pic !== null) {
+        $('.layui-nav-img').attr('src', data.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
